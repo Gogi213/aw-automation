3273522405f699ba7f1f62bc91c273496dbab9e1
Redmi <1993georgiy@gmail.com>
2023-12-01 01:04:07 +0200

    123123

--- config.js
+++ config.js
@@ -2,78 +2,239 @@ const config = {
   // Идентификаторы ресурсов
   folderId: '1BBJSkhFeA67WDOgbRFnrBOb82JqbbMn4',
   spreadsheetId: '1a9qhTubxbdew9IfstH0v7dQijexTBs9jbkkBIIXV-cs',
-
   // Названия листов
   sheetNames: {
     unionDate: 'union_date'
   },
-
   // Типы файлов для обработки
-  fileTypes: ['Video', 'Audio', 'CTV', 'DOOH', 'Display'],
+  fileTypes: ['Video', 'Audio', 'CTV', 'DOOH', 'Display', 'YouTube'],
 
   // Настройки листов
   sheetsData: [
     {
       name: 'Total',
       types: {
-        'Video': {
-          headers: [
-          'Totals by campaign,,,,,,,,,,,,,,,,,,',
-          'Creative,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Book a Visit,Map Directions,Phone Calls,Book a Visit  -  Post View,Map Directions - Post View,Phone Calls  - Post View,In-Store Visits,Total Conversions'
-          ],
+        'GOH / Video': {
+          headers: ['Totals by campaign,,,,,,,,,,,,,,,,,,',
+            'Creative,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Book a Visit,Map Directions,Phone Calls,Book a Visit  -  Post View,Map Directions - Post View,Phone Calls  - Post View,In-Store Visits,Total Conversions'],
+          merge: [1, 1, 1, 19]
+        },
+        'GOH / CTV': {
+          headers: ['Totals by campaign,,,,,,,,,,,,,,,,,,',
+            'Creative,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Book a Visit,Map Directions,Phone Calls,Book a Visit  -  Post View,Map Directions - Post View,Phone Calls  - Post View,In-Store Visits,Total Conversions'],
           merge: [1, 1, 1, 19]
         },
 
-        'Display': {
-          headers: [
-            'Totals by campaign,,,,,,,,,,,,,,,,,,',
-            'Creative,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Book a Visit,Map Directions,Phone Calls,Book a Visit  -  Post View,Map Directions - Post View,Phone Calls  - Post View,In-Store Visits,Total Conversions'
-          ],
+        'GOH / Display': {
+          headers: ['Totals by campaign,,,,,,,,,,,,,,,,,,',
+            'Creative,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Book a Visit,Map Directions,Phone Calls,Book a Visit  -  Post View,Map Directions - Post View,Phone Calls  - Post View,In-Store Visits,Total Conversions'],
           merge: [1, 1, 1, 19]
         },
 
-        'Audio': {
-          headers: [
-            'Totals by campaign,,,,,,,,,,,,,,,,,,',
-            'Campaign,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Book a Visit,Map Directions,Phone Calls,Book a Visit  -  Post View,Map Directions - Post View,Phone Calls  - Post View,In-Store Visits,Total Conversions'
-
-          ],
+        'GOH / Audio': {
+          headers: ['Totals by campaign,,,,,,,,,,,,,,,,,,',
+            'Campaign,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Book a Visit,Map Directions,Phone Calls,Book a Visit  -  Post View,Map Directions - Post View,Phone Calls  - Post View,In-Store Visits,Total Conversions'],
           merge: [1, 1, 1, 19]
         },
 
-        'YouTube': {
-          headers: [
-            'Totals by campaign,,,,,,',
-            'Campaign,Impressions,Views,Clicks,Cost,VCR,CPV'
 
-          ],
+
+        'MOL / Video': {
+          headers: ['Totals by campaign,,,,,,,,,,,,',
+            'Campaign,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,PV Conversions,Total Conversions'],
+          merge: [1, 1, 1, 13]
+        },
+        'MOL / CTV': {
+          headers: ['Totals by campaign,,,,,,,,,,,,',
+            'Campaign,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,PV Conversions,Total Conversions'],
+          merge: [1, 1, 1, 13]
+        },
+
+        'MOL / Display': {
+          headers: ['Totals by campaign,,,,,,,,,',
+            'Campaign,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,PV Conversions,Total Conversions'],
+          merge: [1, 1, 1, 10]
+        },
+
+        'MOL / Audio': {
+          headers: ['Totals by campaign,,,,,,,,,,,,',
+            'Campaign,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,PV Conversions,Total Conversions'],
+          merge: [1, 1, 1, 13]
+        },
+
+
+
+        'SJNY / Video': {
+          headers: ['Totals by campaign,,,,,,,,,,,,,,,,,,,,,',
+            'Campaign,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Brookyln RFI Form PV,Brooklyn Application PV ,Long Island RFI Form PV,Online RFI Form PV,Long Island & Online Application PV,Brookyln RFI Form PC,Brooklyn Application PC,Long Island RFI Form PC,Online RFI Form PC ,Long Island & Online Application PC,Total Conversions'],
+          merge: [1, 1, 1, 22]
+        },
+        'SJNY / CTV': {
+          headers: ['Totals by campaign,,,,,,,,,,,,,,,,,,,,,',
+            'Campaign,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Brookyln RFI Form PV,Brooklyn Application PV ,Long Island RFI Form PV,Online RFI Form PV,Long Island & Online Application PV,Brookyln RFI Form PC,Brooklyn Application PC,Long Island RFI Form PC,Online RFI Form PC ,Long Island & Online Application PC,Total Conversions'],
+          merge: [1, 1, 1, 22]
+        },
+
+        'SJNY / Display': {
+          headers: ['Totals by campaign,,,,,,,,,,,,,,,,,,',
+            'Campaign,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,Brookyln RFI Form PV,Brooklyn Application PV ,Long Island RFI Form PV,Online RFI Form PV,Long Island & Online Application PV,Brookyln RFI Form PC,Brooklyn Application PC,Long Island RFI Form PC,Online RFI Form PC ,Long Island & Online Application PC,Total Conversions'],
+          merge: [1, 1, 1, 19]
+        },
+
+        'SJNY / Audio': {
+          headers: ['Totals by campaign,,,,,,,,,,,,,,,,,,,,,',
+            'Campaign,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Brookyln RFI Form PV,Brooklyn Application PV ,Long Island RFI Form PV,Online RFI Form PV,Long Island & Online Application PV,Brookyln RFI Form PC,Brooklyn Application PC,Long Island RFI Form PC,Online RFI Form PC ,Long Island & Online Application PC,Total Conversions'],
+          merge: [1, 1, 1, 22]
+        },
+
+
+        'Others / Video': {
+          headers: ['Totals by campaign,,,,,,,,,,',
+            'Campaign,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR'],
+          merge: [1, 1, 1, 11]
+        },
+        'Others / CTV': {
+          headers: ['Totals by campaign,,,,,,,,,,',
+            'Campaign,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR'],
+          merge: [1, 1, 1, 11]
+        },
+
+        'Others / Display': {
+          headers: ['Totals by campaign,,,,,,,',
+            'Campaign,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM'],
+          merge: [1, 1, 1, 8]
+        },
+
+        'Others / Audio': {
+          headers: ['Totals by campaign,,,,,,,,,,',
+            'Campaign,Impressions,Viewable Impressions,Unique Inpressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR'],
+          merge: [1, 1, 1, 11]
+        },
+
+
+        // YOUTUBE и DOOH
+        'All YouTube': {
+          headers: ['Totals by campaign,,,,,,',
+            'Campaign,Impressions,Views,Clicks,Cost,VCR,CPV'],
           merge: [1, 1, 1, 7]
         },
 
-        'DOOH': {
-          headers: [
-            'Totals by campaign,,,',
-            'Campaign,Impressions,Cost,CPM'
-          ],
-          merge: [1, 1, 1, 19]
+        'All DOOH': {
+          headers: ['Totals by campaign,,,',
+            'Campaign,Impressions,Cost,CPM'],
+          merge: [1, 1, 1, 4]
         }
-
-
       }
     },
     {
       name: 'Stat by day',
-      headers: [
-        'Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Book a Visit,Map Directions,Phone Calls,Book a Visit  -  Post View,Map Directions - Post View,Phone Calls  - Post View,In-store Visits'
-      ]
+      types: {
+        'GOH / Video': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Book a Visit,Map Directions,Phone Calls,Book a Visit  -  Post View,Map Directions - Post View,Phone Calls  - Post View,In-store Visits']
+        },
+
+        'GOH / CTV': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Book a Visit,Map Directions,Phone Calls,Book a Visit  -  Post View,Map Directions - Post View,Phone Calls  - Post View,In-store Visits']
+        },
+
+        'GOH / Display': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,Book a Visit,Map Directions,Phone Calls,Book a Visit  -  Post View,Map Directions - Post View,Phone Calls  - Post View,In-Store Visits']
+        },
+
+        'GOH / Audio': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Book a Visit,Map Directions,Phone Calls,Book a Visit  -  Post View,Map Directions - Post View,Phone Calls  - Post View,In-store Visits']
+        },
+
+
+
+        'MOL / Video': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,PV Conversions']
+        },
+
+        'MOL / CTV': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,PV Conversions']
+        },
+
+        'MOL / Display': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,PV Conversions']
+        },
+
+        'MOL / Audio': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,PV Conversions']
+        },
+
+
+
+        'SJNY / Video': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Brookyln RFI Form PV,Brooklyn Application PV ,Long Island RFI Form PV,Online RFI Form PV,Long Island & Online Application PV,Brookyln RFI Form PC,Brooklyn Application PC,Long Island RFI Form PC,Online RFI Form PC ,Long Island & Online Application PC']
+        },
+
+        'SJNY / CTV': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Brookyln RFI Form PV,Brooklyn Application PV ,Long Island RFI Form PV,Online RFI Form PV,Long Island & Online Application PV,Brookyln RFI Form PC,Brooklyn Application PC,Long Island RFI Form PC,Online RFI Form PC ,Long Island & Online Application PC']
+        },
+
+        'SJNY / Display': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,Brookyln RFI Form PV,Brooklyn Application PV ,Long Island RFI Form PV,Online RFI Form PV,Long Island & Online Application PV,Brookyln RFI Form PC,Brooklyn Application PC,Long Island RFI Form PC,Online RFI Form PC ,Long Island & Online Application PC']
+        },
+
+        'SJNY / Audio': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR,Brookyln RFI Form PV,Brooklyn Application PV ,Long Island RFI Form PV,Online RFI Form PV,Long Island & Online Application PV,Brookyln RFI Form PC,Brooklyn Application PC,Long Island RFI Form PC,Online RFI Form PC ,Long Island & Online Application PC']
+        },
+
+
+
+
+        'Others / Video': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR']
+        },
+
+        'Others / CTV': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR']
+        },
+
+        'Others / Display': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM']
+        },
+
+        'Others / Audio': {
+          headers: ['Campaign,Creative,Date,Impressions,Viewable Impressions,Unique Impressions,Clicks,CTR,Cost,CPM,VR,Complete views/listens,ACR/VCR']
+        },
+
+
+
+        // YOUTUBE и DOOH
+        'All YouTube': {
+          headers: ['Campaign,Creative,Date,Impressions,Views,Clicks,Cost,VCR,CPV']
+        },
+
+        'ALL DOOH': {
+          headers: ['Campaign,Creative,Date,Impressions,CPM,Cost,Location']
+        },
+
+      }
+    },
+    {
+      name: 'Stat by creatives',
+      types: {
+        'GOH': {
+          headers: ['Creative,Viewable Impressions,Clicks,CTR']
+        },
+        'All - GOH': {
+          headers: ['Creative,Impressions,Clicks,CTR']
+        },
+      }
     },
     {
       name: 'Stat by sites',
-      headers: ['Site,Viewable Impressions,Clicks,CTR']
+      types: {
+        'GOH': {
+          headers: ['Site,Viewable Impressions,Clicks,CTR']
+        },
+        'ALL - GOH': {
+          headers: ['Site,Impressions,Clicks,CTR']
+        },
+      }
     }
-    // Добавьте здесь другие настройки листов, если они есть
   ],
-
-  // Другие общие настройки
-  // ...
 };
+
--- createMultipleSpreadsheets.js
+++ createMultipleSpreadsheets.js
@@ -1,76 +1,116 @@
+
 function createMultipleSpreadsheets() {
-    const uniqueFileNames = getUniqueFileNames();
-  
-    uniqueFileNames.forEach((fileName) => {
-      if (!fileName) return;
-  
-      if (isFileExists(fileName)) {
-        Logger.log(`Файл с именем ${fileName} уже существует.`);
-        return;
-      }
-  
-      const newFile = createNewSpreadsheet(fileName);
-      createSheets(newFile);
-      removeDefaultSheet(newFile);
-      
-      Logger.log(`Файл ${fileName} успешно создан.`);
-    });
-  }
-  
-  function getUniqueFileNames() {
-    const ss = SpreadsheetApp.openById(config.spreadsheetId);
-    const sheet = ss.getSheetByName(config.sheetNames.unionDate);
-    const fileNames = sheet.getRange('AE3:AE' + sheet.getLastRow()).getValues().flat();
-    const fileTypes = sheet.getRange('A3:A' + sheet.getLastRow()).getValues().flat();
-  
-    return [...new Set(fileNames.filter((_, i) => config.fileTypes.includes(fileTypes[i])))];
-  }
-  
-  function isFileExists(fileName) {
-    const folder = DriveApp.getFolderById(config.folderId);
-    const files = folder.getFilesByName(fileName);
-    return files.hasNext();
-  }
-  
-  function createNewSpreadsheet(fileName) {
-    const newFile = SpreadsheetApp.create(fileName);
-    const newFileId = newFile.getId();
-    const file = DriveApp.getFileById(newFileId);
-    const folder = DriveApp.getFolderById(config.folderId);
-  
-    folder.addFile(file);
-    DriveApp.getRootFolder().removeFile(file);
-  
-    return newFile;
-  }
-  
-  function removeDefaultSheet(spreadsheet) {
-    spreadsheet.deleteSheet(spreadsheet.getSheetByName('Sheet1'));
-  }
-  
-  function createSheets(spreadsheet) {
-    config.sheetsData.forEach((sheetData) => {
-      switch (sheetData.name) {
-        case 'Total':
-        case 'Stat by day':
-        case 'Stat by sites':
-          createSheetWithHeaders(spreadsheet, sheetData);
-          break;
-        // Добавьте здесь обработку других типов листов, если они есть
-        default:
-          // Обработка других типов листов
-          break;
-      }
-    });
-  }
-  
-  function createSheetWithHeaders(spreadsheet, sheetData) {
-    const sheet = spreadsheet.insertSheet(sheetData.name);
-    const headers = sheetData.headers.map(header => header.split(','));
-    sheet.getRange(1, 1, headers.length, headers[0].length).setValues(headers).setFontWeight('bold');
-  
-    if (sheetData.merge) {
-      sheet.getRange(...sheetData.merge).merge().setHorizontalAlignment('center');
+  const uniqueFileNames = getUniqueFileNames();
+  uniqueFileNames.forEach((fileName) => {
+    if (!fileName) return;
+    if (isFileExists(fileName)) {
+      Logger.log(`Файл с именем ${fileName} уже существует.`);
+      return;
     }
+    const newFile = createNewSpreadsheet(fileName);
+    createSheets(newFile, getFileType(fileName));
+    removeDefaultSheet(newFile);
+
+    Logger.log(`Файл ${fileName} успешно создан.`);
+  });
+}
+
+function getUniqueFileNames() {
+  const ss = SpreadsheetApp.openById(config.spreadsheetId);
+  const sheet = ss.getSheetByName(config.sheetNames.unionDate);
+  const fileNames = sheet.getRange('AE3:AE' + sheet.getLastRow()).getValues().flat();
+  const fileTypes = sheet.getRange('A3:A' + sheet.getLastRow()).getValues().flat();
+  return [...new Set(fileNames.filter((_, i) => config.fileTypes.includes(fileTypes[i])))];
+}
+
+function isFileExists(fileName) {
+  const folder = DriveApp.getFolderById(config.folderId);
+  const files = folder.getFilesByName(fileName);
+  return files.hasNext();
+}
+
+function createNewSpreadsheet(fileName) {
+  const newFile = SpreadsheetApp.create(fileName);
+  const newFileId = newFile.getId();
+  const file = DriveApp.getFileById(newFileId);
+  const folder = DriveApp.getFolderById(config.folderId);
+  folder.addFile(file);
+  DriveApp.getRootFolder().removeFile(file);
+
+  return newFile;
+}
+
+function removeDefaultSheet(spreadsheet) {
+  spreadsheet.deleteSheet(spreadsheet.getSheetByName('Sheet1'));
+}
+
+function getFileType(fileName) {
+  if (fileName.includes('GOH') && fileName.includes('Video')) return 'GOH / Video';
+  if (fileName.includes('GOH') && fileName.includes('CTV')) return 'GOH / CTV';
+  if (fileName.includes('GOH') && fileName.includes('Display')) return 'GOH / Display';
+  if (fileName.includes('GOH') && fileName.includes('Audio')) return 'GOH / Audio';
+
+  if (fileName.includes('MOL') && fileName.includes('Video')) return 'MOL / Video';
+  if (fileName.includes('MOL') && fileName.includes('CTV')) return 'MOL / CTV';
+  if (fileName.includes('MOL') && fileName.includes('Display')) return 'MOL / Display';
+  if (fileName.includes('MOL') && fileName.includes('Audio')) return 'MOL / Audio';
+
+  if (fileName.includes('SJNY') && fileName.includes('Video')) return 'SJNY / Video';
+  if (fileName.includes('SJNY') && fileName.includes('CTV')) return 'SJNY / CTV';
+  if (fileName.includes('SJNY') && fileName.includes('Display')) return 'SJNY / Display';
+  if (fileName.includes('SJNY') && fileName.includes('Audio')) return 'SJNY / Audio';
+
+  if (!fileName.includes('GOH') && !fileName.includes('SJNY') && !fileName.includes('MOL') && fileName.includes('Video')) return 'Others / Video';
+  if (!fileName.includes('GOH') && !fileName.includes('SJNY') && !fileName.includes('MOL') && fileName.includes('CTV')) return 'Others / CTV';
+  if (!fileName.includes('GOH') && !fileName.includes('SJNY') && !fileName.includes('MOL') && fileName.includes('Display')) return 'Others / Display';
+  if (!fileName.includes('GOH') && !fileName.includes('SJNY') && !fileName.includes('MOL') && fileName.includes('Audio')) return 'Others / Audio';
+
+
+
+  // Упомяну здесь, но по факту в case нужно слева добавить бренд, чтобы возвращало не Total а GOH / Total
+
+
+
+
+
+  if (fileName.includes('YouTube')) return 'YouTube';
+  if (fileName.includes('DOOH')) return 'DOOH';
+  // Добавьте здесь другие условия для различных типов файлов, если необходимо
+}
+
+function createSheets(spreadsheet, fileType) {
+  config.sheetsData.forEach((sheetData) => {
+    switch (sheetData.name) {
+      case 'Total':
+        createSheetWithHeaders(spreadsheet, sheetData, fileType);
+        break;
+      case 'Stat by day':
+        createSheetWithHeaders(spreadsheet, sheetData, fileType);
+        break;
+      case 'Stat by creatives':
+        createSheetWithHeaders(spreadsheet, sheetData, fileType);
+        break;
+      case 'Stat by sites':
+        createSheetWithHeaders(spreadsheet, sheetData, fileType);
+        break;
+    }
+  });
+}
+
+function createSheetWithHeaders(spreadsheet, sheetData, fileType) {
+  let headers, merge;
+  if (sheetData.name === 'Total' && sheetData.types[fileType]) {
+    headers = sheetData.types[fileType].headers;
+    merge = sheetData.types[fileType].merge;
+  } else {
+    headers = sheetData.headers;
+    merge = sheetData.merge;
   }
-  
+
+  const sheet = spreadsheet.insertSheet(sheetData.name);
+  const headerRows = headers.map(header => header.split(','));
+  sheet.getRange(1, 1, headerRows.length, headerRows[0].length).setValues(headerRows).setFontWeight('bold');
+  if (merge) {
+    sheet.getRange(...merge).merge().setHorizontalAlignment('center');
+  }
+}
